30a45223acb5f4b88fd79de880931c38ff443835
SuWagner <suzz@gmx.net>
2018-12-10 14:05:21 +0100

    reset works, but positioning is off

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ var board = [
 var turn;
 var sqrId;
 var user, computer, row, col;
+const ARR_LENGTH = 3;
 
 $(document).ready(function(){
+  resetBoard();
   // checkbox event listnener
   $(".checkBox").click(function(){
     if($(this).is(":checked")){
@@ -21,9 +23,49 @@ $(document).ready(function(){
   // square event listnener
   $(".square").click(function(){
     sqrId = $(this).attr("id");
-    $("#"+sqrId).text(turn);
-    turn = (turn == user) ? computer : user;
+    if($("#"+sqrId).text() == "") {
+      $("#"+sqrId).text(turn);
+      row = getRow();
+      col = getCol();
+      board[row][col] = turn;
+      turn = (turn == user) ? computer : user;
+      console.log(board);
+    }else{
+      alert("Wrong Move");
+    }
+
+
+  });
+
+//reset board
+  $(".reset").click(function(){
+    resetBoard();
   });
 
 
+
 });
+
+//getting row number
+function getRow(){
+  return Math.floor(sqrId / ARR_LENGTH);
+}
+
+// getting col number
+function getCol(){
+  return sqrId % ARR_LENGTH;
+
+}
+
+function checkWinners(){
+
+}
+
+function resetBoard(){
+  $(".reset").click(function(){
+      $(".square").text("");
+      $(".checkBox").prop("checked", false);
+  });
+}
+
+// go on https://www.youtube.com/watch?v=sX0YfsiB-o4
